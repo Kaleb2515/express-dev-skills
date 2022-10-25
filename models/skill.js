b301e3507a86848fbcfaa742398b83d4f8a4b2be
Kaleb2515 <zuhars@icloud.com>
2022-10-25 18:49:59 -0500

    finished with group help

---
+++ models/skill.js
@@ -0,0 +1,10 @@
+const skills = ['HTML', 'CSS', 'Javascript']
+  
+  module.exports = {
+    getAll
+  };
+  
+  function getAll() {
+    return skills;
+  }
+ 
---
+++ routes/skills.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+// Require the controller that exports To-Do CRUD functions
+var skillsCtrl = require('../controllers/skills');
+
+
+router.get('/', skillsCtrl.index);
+
+module.exports = router;
